71cfbc80a3849dee71d93835c6c219058a5eee18
lastiwan89 <59128819+lastiwan89@users.noreply.github.com>
2023-12-17 06:02:32 +0900

    home page mobile done

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,113 +1,88 @@
-import Image from 'next/image'
+import { CEO, TEAMS } from "@/constant";
+import Image from "next/image";
+import React from "react";
 
 export default function Home() {
   return (
-    <main className="flex min-h-screen flex-col items-center justify-between p-24">
-      <div className="z-10 max-w-5xl w-full items-center justify-between font-mono text-sm lg:flex">
-        <p className="fixed left-0 top-0 flex w-full justify-center border-b border-gray-300 bg-gradient-to-b from-zinc-200 pb-6 pt-8 backdrop-blur-2xl dark:border-neutral-800 dark:bg-zinc-800/30 dark:from-inherit lg:static lg:w-auto  lg:rounded-xl lg:border lg:bg-gray-200 lg:p-4 lg:dark:bg-zinc-800/30">
-          Get started by editing&nbsp;
-          <code className="font-mono font-bold">src/app/page.tsx</code>
+    <main>
+      {/* Hero */}
+      <section className="bg-green-midnight px-6 py-10 min-h-[440px] bg-home2 bg-no-repeat bg-bottom">
+        <h3 className="text-center text-white font-bold text-fs-40 leading-lh-40 px-14">
+          Find the best <span className="text-light-coral">talent</span>
+        </h3>
+        <p className="text-center text-white text-fs-15 font-semibold leading-lh-28 mt-6">
+          Finding the right people and building high performing teams can be
+          hard. Most companies aren’t tapping into the abundance of global
+          talent. We’re about to change that.
         </p>
-        <div className="fixed bottom-0 left-0 flex h-48 w-full items-end justify-center bg-gradient-to-t from-white via-white dark:from-black dark:via-black lg:static lg:h-auto lg:w-auto lg:bg-none">
-          <a
-            className="pointer-events-none flex place-items-center gap-2 p-8 lg:pointer-events-auto lg:p-0"
-            href="https://vercel.com?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            By{' '}
-            <Image
-              src="/vercel.svg"
-              alt="Vercel Logo"
-              className="dark:invert"
-              width={100}
-              height={24}
-              priority
-            />
-          </a>
+      </section>
+      {/* Team Section */}
+      <section className="bg-home3 bg-no-repeat bg-top-50p bg-green-sacramento px-6 py-16">
+        <div className="w-[240px]">
+          <div className="bg-light-coral h-1 w-12"></div>
+          <h1 className="mt-8 text-white text-fs-32 leading-lh-32 font-bold">
+            Build & manage distributed teams like no one else.
+          </h1>
         </div>
-      </div>
-
-      <div className="relative flex place-items-center before:absolute before:h-[300px] before:w-[480px] before:-translate-x-1/2 before:rounded-full before:bg-gradient-radial before:from-white before:to-transparent before:blur-2xl before:content-[''] after:absolute after:-z-20 after:h-[180px] after:w-[240px] after:translate-x-1/3 after:bg-gradient-conic after:from-sky-200 after:via-blue-200 after:blur-2xl after:content-[''] before:dark:bg-gradient-to-br before:dark:from-transparent before:dark:to-blue-700 before:dark:opacity-10 after:dark:from-sky-900 after:dark:via-[#0141ff] after:dark:opacity-40 before:lg:h-[360px] z-[-1]">
-        <Image
-          className="relative dark:drop-shadow-[0_0_0.3rem_#ffffff70] dark:invert"
-          src="/next.svg"
-          alt="Next.js Logo"
-          width={180}
-          height={37}
-          priority
-        />
-      </div>
-
-      <div className="mb-32 grid text-center lg:max-w-5xl lg:w-full lg:mb-0 lg:grid-cols-4 lg:text-left">
-        <a
-          href="https://nextjs.org/docs?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800/30"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2 className={`mb-3 text-2xl font-semibold`}>
-            Docs{' '}
-            <span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
-              -&gt;
-            </span>
-          </h2>
-          <p className={`m-0 max-w-[30ch] text-sm opacity-50`}>
-            Find in-depth information about Next.js features and API.
-          </p>
-        </a>
-
-        <a
-          href="https://nextjs.org/learn?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-          className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800/30"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2 className={`mb-3 text-2xl font-semibold`}>
-            Learn{' '}
-            <span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
-              -&gt;
-            </span>
-          </h2>
-          <p className={`m-0 max-w-[30ch] text-sm opacity-50`}>
-            Learn about Next.js in an interactive course with&nbsp;quizzes!
-          </p>
-        </a>
-
-        <a
-          href="https://vercel.com/templates?framework=next.js&utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800/30"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2 className={`mb-3 text-2xl font-semibold`}>
-            Templates{' '}
-            <span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
-              -&gt;
-            </span>
-          </h2>
-          <p className={`m-0 max-w-[30ch] text-sm opacity-50`}>
-            Explore starter templates for Next.js.
-          </p>
-        </a>
-
-        <a
-          href="https://vercel.com/new?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800/30"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2 className={`mb-3 text-2xl font-semibold`}>
-            Deploy{' '}
-            <span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
-              -&gt;
-            </span>
-          </h2>
-          <p className={`m-0 max-w-[30ch] text-sm opacity-50`}>
-            Instantly deploy your Next.js site to a shareable URL with Vercel.
-          </p>
-        </a>
-      </div>
+        <div className="mt-12">
+          {TEAMS.map((team) => (
+            <div
+              className="flex flex-col items-center justify-center"
+              key={team.key}
+            >
+              <Image
+                className="mt-12"
+                src={`${team.url}`}
+                alt={`${team.label} icon`}
+                width={72}
+                height={72}
+              />
+              <div>
+                <h3 className="text-center text-light-coral text-fs-18 leading-lh-28 font-bold my-4">
+                  {team.label}
+                </h3>
+                <p className="text-center text-white text-fs-15 leading-lh-25 font-semibold">
+                  {team.text}
+                </p>
+              </div>
+            </div>
+          ))}
+        </div>
+      </section>
+      {/* CEO section */}
+      <section className="px-6 py-40 custom-home-ceo1">
+        <div>
+          <h3 className="text-center text-white text-fs-32 leading-lh-32 font-bold mb-12">
+            Delivering real results for top companies. Some of our{" "}
+            <span className="text-blue-rapture">success stories.</span>
+          </h3>
+        </div>
+        <div>
+          {CEO.map((item) => (
+            <div
+              className="flex flex-col items-center py-9 bg-quote bg-no-repeat bg-top"
+              key={item.key}
+            >
+              <p className="text-center text-white text-fs-15 leading-lh-25 font-semibold">
+                {item.text}
+              </p>
+              <p className="text-center text-blue-rapture mt-4 text-fs-18 leading-lh-28 font-bold">
+                {item.name}
+              </p>
+              <p className="text-center text-white italic font-medium text-fs-13 leading-lh-18">
+                {item.job}
+              </p>
+              <Image
+                className="rounded-full border-2 border-blue-rapture mt-4 mb-4"
+                src={`${item.url}`}
+                width={62}
+                height={62}
+                alt={`${item.name} avatar`}
+              />
+            </div>
+          ))}
+        </div>
+      </section>
     </main>
-  )
+  );
 }

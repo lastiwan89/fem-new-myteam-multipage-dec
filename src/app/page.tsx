2b759b541d6c6ea076af5d44c7143e2255827bba
lastiwan89 <59128819+lastiwan89@users.noreply.github.com>
2023-12-17 22:30:25 +0900

    home tablet done

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -7,42 +7,44 @@ export default function Home() {
   return (
     <main>
       {/* Hero */}
-      <section className="bg-green-midnight px-6 py-10 min-h-[440px] bg-home2 bg-no-repeat bg-bottom">
-        <h3 className="text-center text-white font-bold text-fs-40 leading-lh-40 px-14">
+      <section className="bg-green-midnight px-6 pt-20 pb-52 bg-home2 bg-no-repeat bg-bottom flex flex-col items-center">
+        <h1 className="text-center text-white font-bold text-fs-40 leading-lh-40 md:text-fs-64 md:leading-lh-56 md:w-[457px] px-14">
           Find the best <span className="text-light-coral">talent</span>
-        </h3>
-        <p className="text-center text-white text-fs-15 font-semibold leading-lh-28 mt-6">
+        </h1>
+        <p className="text-center text-white text-fs-15 font-semibold leading-lh-28 mt-6 md:w-[457px]">
           Finding the right people and building high performing teams can be
           hard. Most companies aren’t tapping into the abundance of global
           talent. We’re about to change that.
         </p>
       </section>
       {/* Team Section */}
-      <section className="bg-home3 bg-no-repeat bg-top-50p bg-green-sacramento px-6 py-16">
-        <div className="w-[240px]">
+      <section className="bg-home3 bg-no-repeat md:custom-team-md bg-top-50p bg-green-sacramento px-6 py-16 md:px-24 md:py-28">
+        <div className="w-[240px] md:w-[445px]">
           <div className="bg-light-coral h-1 w-12"></div>
           <h1 className="mt-8 text-white text-fs-32 leading-lh-32 font-bold">
             Build & manage distributed teams like no one else.
           </h1>
         </div>
-        <div className="mt-12">
+        <div className="mt-12 flex flex-col md:justify-start md:items-start">
           {TEAMS.map((team) => (
             <div
-              className="flex flex-col items-center justify-center"
+              className="flex flex-col md:flex-row md:gap-6 items-center justify-center"
               key={team.key}
             >
-              <Image
-                className="mt-12"
-                src={`${team.url}`}
-                alt={`${team.label} icon`}
-                width={72}
-                height={72}
-              />
-              <div>
-                <h3 className="text-center text-light-coral text-fs-18 leading-lh-28 font-bold my-4">
+              <div className="h-24 w-24 flex items-center my-6">
+                <Image
+                  className="mt-12"
+                  src={`${team.url}`}
+                  alt={`${team.label} icon`}
+                  width={72}
+                  height={72}
+                />
+              </div>
+              <div className="flex flex-col gap-4 md:mb-8 md:justify-center">
+                <h3 className="text-center md:text-start text-light-coral text-fs-18 leading-lh-28 font-bold">
                   {team.label}
                 </h3>
-                <p className="text-center text-white text-fs-15 leading-lh-25 font-semibold">
+                <p className="text-center md:text-start text-white text-fs-15 leading-lh-25 font-semibold">
                   {team.text}
                 </p>
               </div>
@@ -51,7 +53,7 @@ export default function Home() {
         </div>
       </section>
       {/* CEO section */}
-      <section className="px-6 py-40 custom-home-ceo1">
+      <section className="px-6 py-40 md:px-24 md:pt-28 md:pb-20 custom-home-ceo1">
         <div>
           <h3 className="text-center text-white text-fs-32 leading-lh-32 font-bold mb-12">
             Delivering real results for top companies. Some of our{" "}

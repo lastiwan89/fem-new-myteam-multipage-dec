93572356511e2fe4e57433a83e98c83d36d69079
lastiwan89 <59128819+lastiwan89@users.noreply.github.com>
2023-12-20 23:11:24 +0900

    update hamburger menu

--- src/components/navbar/hamburger.tsx
+++ src/components/navbar/hamburger.tsx
@@ -1,4 +1,6 @@
 "use client";
+import Link from "next/link";
+import Button from "../button";
 
 export default function Hamburger({
   isOpen,
@@ -16,13 +18,38 @@ export default function Hamburger({
           </g>
         </svg>
       ) : (
-        <svg xmlns="http://www.w3.org/2000/svg" width="18" height="17">
-          <path
-            fill="#FFF"
-            fill-rule="evenodd"
-            d="M15.01.368l2.122 2.122-6.01 6.01 6.01 6.01-2.122 2.122L9 10.622l-6.01 6.01L.868 14.51 6.88 8.5.87 2.49 2.988.368 9 6.38 15.01.37z"
-          />
-        </svg>
+        <div className="mobile-nav-bg flex flex-col w-80 gap-8 px-12 py-28 fixed top-0 right-0  overflow-y-hidden overflow-x-hidden transition-transform translate-x-4  duration-300 items-start justify-start h-screen bg-blue-police z-40">
+          <svg
+            className="absolute top-12 right-9"
+            xmlns="http://www.w3.org/2000/svg"
+            width="18"
+            height="17"
+          >
+            <path
+              fill="#FFF"
+              fill-rule="evenodd"
+              d="M15.01.368l2.122 2.122-6.01 6.01 6.01 6.01-2.122 2.122L9 10.622l-6.01 6.01L.868 14.51 6.88 8.5.87 2.49 2.988.368 9 6.38 15.01.37z"
+            />
+          </svg>
+          <Link
+            className="text-white hover:text-light-coral text-fs-18 leading-lh-28 font-semibold"
+            href="/"
+          >
+            home
+          </Link>
+          <Link
+            className="text-white hover:text-light-coral text-fs-18 leading-lh-28 font-semibold"
+            href="/about"
+          >
+            about
+          </Link>
+          <Link
+            className="border-2 text-fs-h3-18 font-semibold leading-lh-18 px-8 py-2 rounded-full border-white text-white hover:bg-white hover:text-green-midnight"
+            href={`/contact`}
+          >
+            contact us
+          </Link>
+        </div>
       )}
     </div>
   );

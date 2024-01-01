9a2222640417ce53b973e3a201bd102c8fdffb2f
lastiwan89 <59128819+lastiwan89@users.noreply.github.com>
2024-01-01 20:21:50 +0900

    form types

---
+++ src/types/index.ts
@@ -0,0 +1,15 @@
+export type Form = {
+  name: string;
+  email: string;
+  company: string;
+  title: string;
+  message: string;
+};
+
+export type ValidForm = {
+  name: string;
+  email: string;
+  company: string;
+  title: string;
+  message: string;
+};

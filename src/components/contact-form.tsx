35035eda7c7c7f386e6abb28c1a249e907c34995
lastiwan89 <59128819+lastiwan89@users.noreply.github.com>
2024-01-01 20:22:41 +0900

    completing form data element with error but not tested enough

--- src/components/contact-form.tsx
+++ src/components/contact-form.tsx
@@ -1,14 +1,7 @@
 "use client";
 
+import { Form, ValidForm } from "@/types";
 import React, { useState } from "react";
-
-interface Form {
-  name: string;
-  email: string;
-  company: string;
-  title: string;
-  message: string;
-}
 
 const initialLogin: Form = {
   name: "",
@@ -18,16 +11,50 @@ const initialLogin: Form = {
   message: "",
 };
 
+const initialFormValid: ValidForm = {
+  name: "",
+  email: "",
+  company: "",
+  title: "",
+  message: "",
+};
+
 export default function ContactForm() {
   const [formData, setFormData] = useState<Form>(initialLogin);
+  const [isValid, setIsValid] = useState<ValidForm>(initialFormValid);
 
-  function handleChange(event: React.ChangeEvent<HTMLInputElement>) {
-    const { name, value } = event.target;
-    setFormData({ ...formData, [name]: value });
+  function handleChange(e: React.ChangeEvent<HTMLInputElement>) {
+    const { name, value } = e.target;
+    setFormData((prevState) => {
+      return {
+        ...prevState,
+        [name]: value,
+      };
+    });
   }
 
-  function handleSubmit(event: React.FormEvent<HTMLFormElement>) {
-    event.preventDefault();
+  function handleSubmit(e: React.FormEvent<HTMLFormElement>) {
+    const regex: RegExp = /^[\w-\.]+@([\w-]+\.)+[\w-]{2,4}$/g;
+    e.preventDefault();
+    const isError: ValidForm = initialFormValid;
+    if (!formData.name.trim()) {
+      isError.name = "name is required";
+    }
+    if (!formData.email.trim()) {
+      isError.email = "email is required";
+    } else if (!regex.test(formData.email)) {
+      isError.email = "email is not valid!";
+    }
+    if (!formData.company.trim()) {
+      isError.company = "company is required";
+    }
+    if (!formData.title.trim()) {
+      isError.title = "title is required";
+    }
+    if (!formData.message.trim()) {
+      isError.message = "message is required";
+    }
+    setIsValid(isError);
     console.log(formData);
     setFormData(initialLogin);
   }
@@ -38,50 +65,85 @@ export default function ContactForm() {
       className="form-bg1 md:contact-md2 bg-green-midnight p-6 pb-24 md:px-32 lg:min-w-[50%] lg:px-6 lg:py-28 lg:pr-40"
     >
       <div className="mb-6 flex flex-col gap-y-2 lg:min-w-full lg:gap-y-5">
-        <input
-          className="w-full border-b-2 border-white px-2 py-2 text-fs-15 font-semibold leading-lh-25 lg:py-4"
-          type="text"
-          name="name"
-          id="name"
-          placeholder="Name"
-          onChange={handleChange}
-          value={formData.name}
-        />
-        <input
-          className="w-full border-b-2 border-white px-2 py-2 text-fs-15 font-semibold leading-lh-25 lg:py-4"
-          type="email"
-          name="email"
-          id="email"
-          placeholder="Email Address"
-          onChange={handleChange}
-          value={formData.email}
-        />
-        <input
-          className="w-full border-b-2 border-white px-2 py-2 text-fs-15 font-semibold leading-lh-25 lg:py-4"
-          type="text"
-          name="company"
-          id="company"
-          placeholder="Company Name"
-          onChange={handleChange}
-          value={formData.company}
-        />
-        <input
-          className="w-full border-b-2 border-white px-2 py-2 text-fs-15 font-semibold leading-lh-25 lg:py-4"
-          type="text"
-          name="title"
-          id="title"
-          placeholder="Title"
-          onChange={handleChange}
-          value={formData.title}
-        />
-        <input
-          className="w-full border-b-2 border-white px-2 pb-12 pt-2 text-fs-15 font-semibold leading-lh-25"
-          name="message"
-          id="message"
-          placeholder="Message"
-          onChange={handleChange}
-          value={formData.message}
-        />
+        <div className="flex flex-col">
+          <input
+            className="w-full border-b-2 border-white px-2 py-2 text-fs-15 font-semibold leading-lh-25 lg:py-4"
+            type="text"
+            name="name"
+            id="name"
+            placeholder="Name"
+            onChange={handleChange}
+            value={formData.name}
+          />
+          {isValid && (
+            <p className="text-end text-fs-13 text-light-coral">
+              {isValid.name}
+            </p>
+          )}
+        </div>
+        <div className="flex flex-col">
+          <input
+            className="w-full border-b-2 border-white px-2 py-2 text-fs-15 font-semibold leading-lh-25 lg:py-4"
+            type="email"
+            name="email"
+            id="email"
+            placeholder="Email Address"
+            onChange={handleChange}
+            value={formData.email}
+          />
+          {isValid && (
+            <p className="text-end text-fs-13 text-light-coral">
+              {isValid.email}
+            </p>
+          )}
+        </div>
+        <div className="flex flex-col">
+          <input
+            className="w-full border-b-2 border-white px-2 py-2 text-fs-15 font-semibold leading-lh-25 lg:py-4"
+            type="text"
+            name="company"
+            id="company"
+            placeholder="Company Name"
+            onChange={handleChange}
+            value={formData.company}
+          />
+          {isValid && (
+            <p className="text-end text-fs-13 text-light-coral">
+              {isValid.company}
+            </p>
+          )}
+        </div>
+        <div className="flex flex-col">
+          <input
+            className="w-full border-b-2 border-white px-2 py-2 text-fs-15 font-semibold leading-lh-25 lg:py-4"
+            type="text"
+            name="title"
+            id="title"
+            placeholder="Title"
+            onChange={handleChange}
+            value={formData.title}
+          />
+          {isValid && (
+            <p className="text-end text-fs-13 text-light-coral">
+              {isValid.title}
+            </p>
+          )}
+        </div>
+        <div className="flex flex-col">
+          <input
+            className="w-full border-b-2 border-white px-2 pb-12 pt-2 text-fs-15 font-semibold leading-lh-25"
+            name="message"
+            id="message"
+            placeholder="Message"
+            onChange={handleChange}
+            value={formData.message}
+          />
+          {isValid && (
+            <p className="text-end text-fs-13 text-light-coral">
+              {isValid.message}
+            </p>
+          )}
+        </div>
       </div>
       <button
         className="text-fs-h3-18 rounded-full border-2 border-white bg-white px-8 py-2 font-semibold leading-lh-18"
